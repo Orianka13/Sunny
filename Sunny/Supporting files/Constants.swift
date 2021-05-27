22a88ced7c8c690202b79249080b179904b3616a
Olesia <odobrogor@gmail.com>
2021-05-27 21:04:30 +0300

    Initial Commit

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Олеся Егорова on 27.05.2021.
+//  Copyright © 2021 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "47b4e204e115c7a03f5ec53aee2d63a4"
